8a55e06d2328cd389f9715d5b96cd8202b2d5ff7
valdeezzee <jvalderrama91@knights.ucf.edu>
2017-06-19 01:50:28 -0400

    Start repo

---
+++ Assets/Scripts/HandPositionSetter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.VR;
+
+public class HandPositionSetter : NetworkBehaviour
+{
+
+    IEnumerator TrackNodeCoroutine(VRNode node)
+    {
+        while (true)
+        {
+            transform.rotation = InputTracking.GetLocalRotation(node);
+            transform.position = InputTracking.GetLocalPosition(node);
+            yield return null;
+        }
+    }
+
+    [ClientRpc]
+    public void RpcSetHand(VRNode node)
+    {
+        StartCoroutine(TrackNodeCoroutine(node));
+    }
+}
---
+++ Assets/Scripts/MirrorNetworkedVRNode.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using UnityEngine.VR;
+
+public class MirrorNetworkedVRNode : NetworkBehaviour {
+    public VRNode node = VRNode.LeftHand;
+    public GameObject handPositionSetterPrefab;
+
+    public void Start()
+    {   
+    }
+
+    [ContextMenu("Change Scene")]
+    private void ChangeScene()
+    {
+        foreach(var networkBehaviour in FindObjectsOfType<NetworkBehaviour>())
+        {
+            DontDestroyOnLoad(networkBehaviour.gameObject);
+        }
+        SceneManager.LoadScene(1);
+    }
+
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        StartCoroutine(TrackHeadCoroutine());
+        StartCoroutine(MakeSureSetHand());
+    }
+
+    IEnumerator MakeSureSetHand()
+    {
+        if (NetworkServer.active)
+        {
+            var left = Instantiate(handPositionSetterPrefab);
+            var right = Instantiate(handPositionSetterPrefab);
+            NetworkServer.SpawnWithClientAuthority(left, connectionToClient);
+            NetworkServer.SpawnWithClientAuthority(right, connectionToClient);
+
+            yield return new WaitForSeconds(1f);
+            left.GetComponent<HandPositionSetter>().RpcSetHand(VRNode.LeftHand);
+            right.GetComponent<HandPositionSetter>().RpcSetHand(VRNode.RightHand);
+        }
+    }
+
+    IEnumerator TrackHeadCoroutine()
+    {
+        while(true)
+        {
+            transform.rotation = InputTracking.GetLocalRotation(node);
+            transform.position = InputTracking.GetLocalPosition(node);
+            yield return null;
+        }
+    }
+}
